8ae09db3b5d76d38520f7c145a765f8b9fe7cb8b
Rachel Sacdalan <rachelsacdalan@Rachels-MacBook-Air.local>
2022-04-02 16:45:13 -0400

    Created Initial Repo

---
+++ MedTech/MedTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedTech
+//
+//  Created by Rachel Sacdalan on 4/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MedTech/MedTechUITests/MedTechUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MedTechUITestsLaunchTests.swift
+//  MedTechUITests
+//
+//  Created by Rachel Sacdalan on 4/2/22.
+//
+
+import XCTest
+
+class MedTechUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
